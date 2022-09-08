7caa39aee893be80d099a6879ee195e678d34208
Fabricio Monteiro Dos Santos <105099322+LenorSantos@users.noreply.github.com>
2022-09-08 12:07:04 -0300

    App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import {useState, useRef} from "react";
 import axios from "axios";
 
 function App() {
-  const refdestaques = useRef();
-  const refpromo = useRef();
+  const refnews = useRef();
+  const refproducts = useRef();
 
   // toppoage
   const [hora, sethora] = useState("");
   const [data, setdata] = useState("");
 
   const sendtoppage = () => {
-    axios.post("http://localhost:3001/toppage", {
+    axios.post("http://localhost:3001/datetime", {
       hora: hora,
       data: data,
     });
   }
   // toppage
 
-  // promo
+  // products
   const [id, setid] = useState(0);
   const [link, setlink] = useState("");
   const [porcentagem, setporcentagem] = useState("");
@@ -28,19 +28,19 @@ function App() {
   // send image
   const [nameimage, setnameimage] = useState("");
   const [image, setimage] = useState(null);
-  const imgpromo = new FormData();
+  const imgproducts = new FormData();
 
   // data
   const [pulldados, setpulldados] = useState([]);
 
   const senddados = () => {
-    imgpromo.append("image", image);
-    axios.post("http://localhost:3001/image", imgpromo).then((result) => {
+    imgproducts.append("image", image);
+    axios.post("http://localhost:3001/image", imgproducts).then((result) => {
       if (result.data === "TypeError: imgnew.mv is not a function") {
         console.log(result);
-        refdestaques.current.value = "";
+        refproducts.current.value = "";
       } else if (result.status === 200) {
-        axios.post("http://localhost:3001/promo", {
+        axios.post("http://localhost:3001/products", {
           id: parseInt(id),
           link: link,
           porcentagem: porcentagem,
@@ -49,10 +49,10 @@ function App() {
           imgname: nameimage
         }).then((result) => {
           if (result.data !== "") {
-            refpromo.current.value = "";
+            refproducts.current.value = "";
             console.log(result);
           } else if (result.status === 200) {
-            refpromo.current.value = "";
+            refproducts.current.value = "";
             getdados();
           }
         });
@@ -61,7 +61,7 @@ function App() {
   }
 
   const getdados = () => {
-    axios.get("http://localhost:3001/pullpromo").then((result) => {
+    axios.get("http://localhost:3001/reqproducts").then((result) => {
       setpulldados(result.data);
     }).catch((error) => {
       console.log(error);
@@ -69,45 +69,45 @@ function App() {
   }
 
   const deletedados = (val) => {
-    axios.post("http://localhost:3001/deletepromo", val).then((result) => {
+    axios.post("http://localhost:3001/deleteproducts", val).then((result) => {
       if (result.status === 200) {
         getdados();
       }
     });
   }
-  // promo
+  // products
 
-  // destaques
-  const [idnew, setidnew] = useState(0);
+  // news
+  const [idnews, setidnews] = useState(0);
   const [texto, settexto] = useState("");
 
   // send image
-  const [nameimgnew, setnameimgnew] = useState("");
+  const [nameimgnews, setnameimgnews] = useState("");
   const [imgnew, setimgnew] = useState(null);
-  const imgdestaques = new FormData();
+  const imgnews = new FormData();
 
   // data
-  const [pulldestaques, setpulldestaques] = useState([]);
-  console.log(pulldestaques);
+  const [pullnews, setpullnews] = useState([]);
+  console.log(pullnews);
 
   const senddadosdestaque = () => {
-    imgdestaques.append("imgnew", imgnew);
-    axios.post("http://localhost:3001/imgdestaques", imgdestaques).then((result) => {
+    imgnews.append("imgnew", imgnew);
+    axios.post("http://localhost:3001/imgnews", imgnews).then((result) => {
       if (result.data === "TypeError: imgnew.mv is not a function") {
         console.log(result);
-        refdestaques.current.value = "";
+        refnews.current.value = "";
       } else if (result.status === 200) {
-        axios.post("http://localhost:3001/destaques", {
-          id: parseInt(idnew),
+        axios.post("http://localhost:3001/news", {
+          id: parseInt(idnews),
           texto: texto,
-          imgname: nameimgnew
+          imgname: nameimgnews
         }).then((result) => {
           if (result.data !== "") {
-            refdestaques.current.value = "";
+            refnews.current.value = "";
             console.log(result);
           } else if (result.status === 200) {
-            refdestaques.current.value = "";
-            getdestaques();
+            refnews.current.value = "";
+            getnews();
           }
         });
       }
@@ -115,26 +115,26 @@ function App() {
     });
   }
 
-  const getdestaques = () => {
-    axios.get("http://localhost:3001/pulldestaques").then((result) => {
-      setpulldestaques(result.data);
+  const getnews = () => {
+    axios.get("http://localhost:3001/pullnews").then((result) => {
+      setpullnews(result.data);
     }).catch((error) => {
       console.log(error);
     });
   }
 
-  const deldestaques = (val) => {
-    axios.post("http://localhost:3001/deldestaques", val).then((result) => {
+  const delnews = (val) => {
+    axios.post("http://localhost:3001/delnews", val).then((result) => {
       if (result.status === 200) {
-        getdestaques();
+        getnews();
       }
     });
   }
-  // destaques
+  // news
 
   // auto request
-  if (pulldestaques.length === 0) {
-    getdestaques();
+  if (pullnews.length === 0) {
+    getnews();
   }
   if (pulldados.length === 0) {
     getdados();
@@ -162,9 +162,9 @@ function App() {
       </div>
       {/* update time */}
       <div className="line"></div>
-      {/* inicio promo */}
+      {/* inicio products */}
       <div className="dados">
-        <h1>Promoção</h1>
+        <h1>productsção</h1>
         <label>ID:</label>
         <input type="number" onChange={(event) => {
           setid(event.target.value);
@@ -195,7 +195,7 @@ function App() {
         <input type="file" onChange={(event) => {
           setnameimage(event.target.files[0].name);
           setimage(event.target.files[0]);
-        }} ref={refpromo}/>
+        }} ref={refproducts}/>
         <div className="btndados">
           <button onClick={senddados}>Enviar Dados</button>
         </div>
@@ -221,17 +221,17 @@ function App() {
             );
           })}
         </div>
-      <p>Promoção</p>
+      <p>productsção</p>
       <button onClick={getdados}>Puxar dados</button>
       </div>
-      {/* fim promo */}
+      {/* fim products */}
       <div className="line"></div>
       {/* inicio destques */}
       <div className="dados-destaque">
-        <h1>Destaques</h1>
+        <h1>news</h1>
         <label>ID:</label>
         <input type="number" onChange={(event) => {
-          setidnew(event.target.value);
+          setidnews(event.target.value);
         }}
         />
         <label>Texto:</label>
@@ -240,29 +240,29 @@ function App() {
         }}></textarea>
         <label>Imagem</label>
         <input type="file" onChange={(event) => {
-          setnameimgnew(event.target.files[0].name);
+          setnameimgnews(event.target.files[0].name);
           setimgnew(event.target.files[0]);
-        }} ref={refdestaques}/>
+        }} ref={refnews}/>
         <div className="btndados-destaque">
           <button onClick={senddadosdestaque}>Enviar Dados</button>
         </div>
       </div>
-      <div className="pulldestaques">
-        <div className="container-destaques">
-          {Array.from(pulldestaques).map((val) => {
+      <div className="pullnews">
+        <div className="container-news">
+          {Array.from(pullnews).map((val) => {
             return (
-              <div className="destaques">
-                <button onClick={() => {deldestaques(val)}}>apagar</button>
+              <div className="news">
+                <button onClick={() => {delnews(val)}}>apagar</button>
                 <p>{val.id}</p>
                 <img src={val.imgdata} alt="imgpng"/>
               </div>
             );
           })}
         </div>
-        <p>Destaques</p>
-        <button onClick={getdestaques}>Puxar dados</button>
+        <p>news</p>
+        <button onClick={getnews}>Puxar dados</button>
       </div>
-      {/* fim destaques */}
+      {/* fim news */}
     </div>
   );
 }
